677fda16a47560bc998d7751e56b2ed7cd3d25ed
toluwase <toluwasethomas1@gmail.com>
2020-12-29 08:54:34 +0100

    ARRAYS IN JAVA

---
+++ src/com/company/arrays.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class arrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+        intArray[0] = 11;
+        intArray[1] = 4;
+        intArray[2] = 5;
+        intArray[3] = 6;
+        intArray[4]=45;
+        intArray[5]=65;
+        intArray[6]=45;
+        //intArray[7]=56;
+        //System.out.println(intArray);
+        for (int i=0; i<intArray.length; i++ ) {
+            System.out.println(intArray);
+        }
+    }
+}
